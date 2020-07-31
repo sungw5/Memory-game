e6244850af8598ab27084189b78d441aa67c0b7c
Sung Woo Oh <sungbr95@gmail.com>
2020-07-31 00:12:23 -0400

    button pressed animation added

--- game.js
+++ game.js
@@ -1,8 +1,16 @@
+////////////////////////////////////////////////////////
+////////////////////// Variables ///////////////////////
+////////////////////////////////////////////////////////
+
 let buttonColors = ["red", "blue", "green", "yellow", "white"];
 // clicked colors go in
 let userClickedPattern = [];
 // random colors go in
 let gamePattern = [];
+
+////////////////////////////////////////////////////////
+////////////////////// Functions ///////////////////////
+////////////////////////////////////////////////////////
 
 function nextSequence() {
   let rnum = Math.floor(Math.random() * 5);
@@ -13,6 +21,8 @@ function nextSequence() {
   $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
   // random color sound played
   playSound(randomChosenColor);
+  // add pressed animation
+  animatePress(randomChosenColor);
 }
 
 function clickColor() {
@@ -20,12 +30,26 @@ function clickColor() {
   userClickedPattern.push(userChosenColor);
   // clicked color sound played
   playSound(userChosenColor);
+  // add pressed animation
+  animatePress(userChosenColor);
 }
 
 function playSound(name) {
   let audio = new Audio(`./sounds/${name}.mp3`);
   audio.play();
 }
+
+function animatePress(currentColor) {
+  $(`.${currentColor}`).addClass("pressed");
+  //   remove class after 100 ms
+  setTimeout(function () {
+    $(`.${currentColor}`).removeClass("pressed");
+  }, 100);
+}
+
+////////////////////////////////////////////////////////
+///////////////////////// MAIN /////////////////////////
+////////////////////////////////////////////////////////
 
 function init() {
   nextSequence();
